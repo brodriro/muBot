62197dc1ef5d71cbc7b99688bacb74f768c84eb8
brodriro <rzknairb@gmail.com>
2024-09-06 21:45:33 -0500

    test message reply

--- src/commands/mu.ts
+++ src/commands/mu.ts
@@ -8,10 +8,10 @@ module.exports = {
     if(args.length < 1) return message.reply('Comando invalido');
 
     let response = ''
-    if(args.includes('panda')) response = 'Panda o PANDAWAN es un offero legendario, solo tienes que darle un pj lleno de items y joyas para que lo queme todo y desinstale el juego';
+    if(args.includes('panda')) response = '';
 
-    if(args.includes('akus')) response = 'Akus.. No lo llames para activar en bosses porque se pone a gritar como loca';
-    if(args.includes('toguro')) response = 'Toguro o Treguaguro para los amigos, le gusta bossear.. siempre esta disponible cuando puede, grita como loca por el TP, se desconecta y dice que todos somos una mierda'
+    if(args.includes('akus')) response = '';
+    if(args.includes('toguro')) response = ''
 
     message.reply(response);
   }
